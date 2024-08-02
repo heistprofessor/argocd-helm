a4cacc4f7f2e132343a933c7eb7db071500d8a0d
Jeet9009 <jeetp@cloudthat.com>
2024-08-02 15:12:13 +0530

    updates

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,5 +4,13 @@
 cd /var/www/html/deployment/dist/
 chmod +x server.js
 echo "Starting the application..."
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install --lts
+node -e "console.log('Running Node.js ' + process.version)"
+npm install pm2 -g
+pm2 completion install
+pm2 update
 pm2 startup
 pm2 start /var/www/html/deployment/dist/server.js
