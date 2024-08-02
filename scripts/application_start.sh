17a8519da1e5efd279bf80f6691d141a5b48be1a
Jeet9009 <jeetp@cloudthat.com>
2024-08-02 16:03:51 +0530

    updates

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,15 +3,15 @@
 
 cd /var/www/html/deployment/dist/
 chmod +x server.js
-# yum update -y
-# yum upgrade -y
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-# source ~/.bashrc
-# nvm install --lts
-# node -e "console.log('Running Node.js ' + process.version)"
-# npm install pm2 -g
-# pm2 startup
-# pm2 save
+yum update -y
+yum upgrade -y
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+source ~/.bashrc
+nvm install --lts
+node -e "console.log('Running Node.js ' + process.version)"
+npm install pm2 -g
+pm2 startup
+pm2 save
 echo "Starting the application..."
 pm2 start /var/www/html/deployment/dist/server.js
 pm2 save
--- scripts/download_bundle.sh
+++ scripts/download_bundle.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "No Download bundle script as of now..."
-yum update -y
-yum upgrade -y
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-source ~/.bashrc
-nvm install --lts
-node -e "console.log('Running Node.js ' + process.version)"
-npm install pm2 -g
-pm2 startup
-pm2 save
+# yum update -y
+# yum upgrade -y
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+# source ~/.bashrc
+# nvm install --lts
+# node -e "console.log('Running Node.js ' + process.version)"
+# npm install pm2 -g
+# pm2 startup
+# pm2 save
