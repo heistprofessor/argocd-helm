5a5e5a0c9bd2955b0bba59a1e921c77d2bdf3a4a
Jeet9009 <jeetp@cloudthat.com>
2024-08-02 15:57:35 +0530

    updates

--- scripts/download_bundle.sh
+++ scripts/download_bundle.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 echo "No Download bundle script as of now..."
-# yum update -y
-# yum upgrade -y
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-# export NVM_DIR="$HOME/.nvm"
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-# nvm install --lts
-# node -e "console.log('Running Node.js ' + process.version)"
-# npm install pm2 -g
-# pm2 startup
-# pm2 save
+yum update -y
+yum upgrade -y
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+source ~/.bashrc
+nvm install --lts
+node -e "console.log('Running Node.js ' + process.version)"
+npm install pm2 -g
+pm2 startup
+pm2 save
